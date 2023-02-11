8ae95fd5557d05263a1af77cdb23e30371670880
Giuliano Assaggio <124878741+giulianoassaggio@users.noreply.github.com>
2023-02-11 22:54:56 +0000

    Nuovo capitolo: transazioni

---
+++ 2. Network/2. Transazioni.py
@@ -0,0 +1,5 @@
+"""
+Le transazioni sono il cuore di Bitcoin. 
+In parole povere, sono trasferimenti di valore da un'entità a un'altra. 
+Vedremo nel Capitolo 6 come le "entità" in questo caso siano in realtà smart contract, ma stiamo anticipando più del dovuto. 
+Per ora vediamo cosa sono le transazioni in Bitcoin, come appaiono e come vengono analizzate.
